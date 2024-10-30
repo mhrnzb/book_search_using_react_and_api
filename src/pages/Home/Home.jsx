7067c43a48d8793bb5d2a44f3c7876372c6bac04
mhrnzb <mehrnazbg1381@gmail.com>
2024-10-30 15:12:29 +0330

    styles added to index.css

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,14 @@
 import React from "react";
+import Header from "../../components/Header/Header";
+import { Outlet } from "react-router-dom";
 const Home=()=>{
 
     return(
         <div>
+            <main>
+                <Header/>
+                <Outlet/>
+            </main>
             
         </div>
     )
