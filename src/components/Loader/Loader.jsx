f468426fa534da2a0a3939f3885f949fe3fb06f3
mhrnzb <mehrnazbg1381@gmail.com>
2024-10-31 13:38:02 +0330

    loader.jsx and loader.css implemented

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,11 +1,13 @@
-import React from "react";
-const Loader=()=>{
+import React from 'react';
+import LoaderImg from "../../images/loader.svg";
+import "./Loader.css";
 
-    return(
-        <div>
-            
-        </div>
-    )
-
+const Loader = () => {
+  return (
+    <div className='loader flex flex-c'>
+      <img src = {LoaderImg} alt = "loader" />
+    </div>
+  )
 }
-export default Loader;
+
+export default Loader
